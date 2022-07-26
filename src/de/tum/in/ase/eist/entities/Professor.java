a174fb361fa05f83fdad6917c7802c47fc1311cc
Emi <emi.mano@tum.de>
2022-07-26 10:29:04 +0200

    abgabe

--- src/de/tum/in/ase/eist/entities/Professor.java
+++ src/de/tum/in/ase/eist/entities/Professor.java
@@ -2,17 +2,17 @@ package de.tum.in.ase.eist.entities;
 
 public class Professor {
 
-    private final String persNr;
-    private final String name;
-    private final int age;
-    private final int semester;
-    private final String email;
+    private  String persNr;
+    private  String name;
+    private  int age;
+    private  int semester;
+    private  String email;
 
     // TODO 1.1 : - Bob is not good at Object Orientated Programming - fix it
 
     // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
 
-    public Professor(final String name, final int age,final String email,final String persNr, final int semester) {
+    public Professor( String name,  int age, String email, String persNr, int semester) {
         this.persNr = persNr;
         this.name = name;
         this.age = age;
--- src/de/tum/in/ase/eist/entities/Student.java
+++ src/de/tum/in/ase/eist/entities/Student.java
@@ -3,18 +3,18 @@ package de.tum.in.ase.eist.entities;
 public class Student {
 
 
-    private final  String matrNr;
-    private final String name;
-    private final int age;
-    private final int semester;
-    private final String email;
+    private   String matrNr;
+    private  String name;
+    private  int age;
+    private  int semester;
+    private  String email;
 
     // TODO 1.1 : Bob is not good at Object Orientated Programming - fix it
   	// TODO 1.1 : Instead of implementing void setSemester(int i), implement void increaseSemester()
 
     // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
 
-    public Student(final String name,final int age,final String email, final String matrNr,final int semester) {
+    public Student( String name, int age, String email,  String matrNr, int semester) {
         this.matrNr = matrNr;
         this.name = name;
         this.age = age;
