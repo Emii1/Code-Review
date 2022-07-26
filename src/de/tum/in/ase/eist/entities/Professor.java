29cb7fa76e35dbdc4c02b40c14b28494d10c3380
Emi <emi.mano@tum.de>
2022-07-26 10:17:19 +0200

    abgabe

--- src/de/tum/in/ase/eist/entities/Professor.java
+++ src/de/tum/in/ase/eist/entities/Professor.java
@@ -2,20 +2,21 @@ package de.tum.in.ase.eist.entities;
 
 public class Professor {
 
-    private String persNr;
-    public static String name;
-    private static int age;
-    private int semester;
-    private String email;
+    private final String persNr;
+    private final String name;
+    private final int age;
+    private final int semester;
+    private final String email;
 
     // TODO 1.1 : - Bob is not good at Object Orientated Programming - fix it
 
     // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
 
-    public Professor(String name, int age, String email, String persNr) {
+    public Professor(final String name, final int age,final String email,final String persNr, final int semester) {
         this.persNr = persNr;
-        Professor.name = name;
-        Professor.age = age;
+        this.name = name;
+        this.age = age;
         this.email = email;
+        this.semester = semester;
     }
 }
--- src/de/tum/in/ase/eist/entities/Student.java
+++ src/de/tum/in/ase/eist/entities/Student.java
@@ -3,25 +3,28 @@ package de.tum.in.ase.eist.entities;
 public class Student {
 
 
-    private String matrNr;
-    public static String name;
-    private static int age;
-    private int semester;
-    private String email;
+    private final  String matrNr;
+    private final String name;
+    private final int age;
+    private final int semester;
+    private final String email;
 
     // TODO 1.1 : Bob is not good at Object Orientated Programming - fix it
   	// TODO 1.1 : Instead of implementing void setSemester(int i), implement void increaseSemester()
 
     // TODO 1.2 : Refactoring - Apply the template method design pattern on Student and Professor to prevent duplicated code
 
-    public Student(String name, int age, String email, String matrNr, int semester) {
+    public Student(final String name,final int age,final String email, final String matrNr,final int semester) {
         this.matrNr = matrNr;
-        Student.name = name;
-        Student.age = age;
+        this.name = name;
+        this.age = age;
         this.semester = semester;
         this.email = email;
     }
 
+    public void increaseSemester(){
+
+    }
     public String getMatrNr() {
         return matrNr;
     }
